dff023d32b8ecbb691ba2af49a86aecc3a706bc3
Ushinji <siguma971@gmail.com>
2020-10-22 20:17:53 +0900

    Add Icon using material-icons

---
+++ src/components/atoms/Icon/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <i class="material-icons">
+    <slot />
+  </i>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({});
+</script>
