d6a49312aac7b3981592fbe285bb641df01ebce8
Ushinji <siguma971@gmail.com>
2020-10-24 15:32:19 +0900

    Add DatePicker

---
+++ src/components/molecules/DatePicker/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Datepicker :value="value" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Datepicker from 'vue3-datepicker';
+
+export default defineComponent({
+  components: { Datepicker },
+  props: {
+    value: { type: Date, required: false, default: new Date() },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../../../../node_modules/vue3-datepicker/dist/vue3-datepicker.css';
+</style>
